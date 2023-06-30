4cf04323f0823606bef632ff42254245a23190d3
SharonAnyona <sharonanyo@gmail.com>
2023-06-30 10:50:49 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,36 +1,28 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
  * rot13 - encodes a string using rot13
  * @s: input string.
  * Return: the pointer to dest.
  */
+
 char *rot13(char *s)
 {
-    int count = 0, i;
-    char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-    while (*(s + count) != '\0')
-    {
-        int found = 0;
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + count) == alphabet[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
 
-        for (i = 0; i < 52; i++)
-        {
-            if (*(s + count) == alph[i])
-            {
-                *(s + count) = rot13[i];
-                found = 1;
-                break;
-            }
-        }
-
-        if (!found)
-            break;
-
-        count++;
-    }
-
-    return s;
+	return (s);
 }
